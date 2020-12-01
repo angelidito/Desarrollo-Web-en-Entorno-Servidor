67f6ae64e499889f3b9afb26858409066e282c0d
Mori <64525549+angelidito@users.noreply.github.com>
2020-12-01 12:21:48 +0100

    1-12

--- UT5-Objetos/becados.php
+++ UT5-Objetos/becados.php
@@ -1,15 +1,16 @@
 <?php
 
-class Alumno {
-	const CURSO ="Segundo DAW";
+class Alumno
+{
+	const CURSO = "Segundo DAW";
 	protected $nom;
 	protected $edad;
 	protected $cali;
-	function __construct($nombre,$e,$nota)
+	function __construct($nombre, $años, $nota)
 	{
-		$this->nom=$nombre;
-		$this->edad=$e;
-		$this->cali=$nota;
+		$this->nom = $nombre;
+		$this->edad = $años;
+		$this->cali = $nota;
 	}
 	function getNombre()
 	{
@@ -17,15 +18,15 @@ class Alumno {
 	}
 	function setNombre($nombre)
 	{
-		$this->nom=$nombre;
-	}	
+		$this->nom = $nombre;
+	}
 	function getEdad()
 	{
 		return $this->edad;
 	}
 	function setEdad($e)
 	{
-		$this->edad=$e;
+		$this->edad = $e;
 	}
 	function getCali()
 	{
@@ -33,44 +34,40 @@ class Alumno {
 	}
 	function setCali($nota)
 	{
-		$this->cali=$nota;
+		$this->cali = $nota;
 	}
-	/*final */function aprobar()
+	/*final */
+	function aprobar()
 	{
-		if ($this->cali>=5)
+		if ($this->cali >= 5)
 			$this->visualizar();
-	}	
+	}
 	function visualizar()
 	{
-		echo $this::CURSO." ";
-		echo $this->nom." ";
-		echo $this->edad." ";
-		echo $this->cali." ";
-		
-	}	
+		echo $this::CURSO . " ";
+		echo $this->nom . " ";
+		echo $this->edad . " ";
+		echo $this->cali . " ";
+	}
 }	// fin de la clase Alumno
 
 class Becado extends Alumno
 {
 	private $importebeca;
 
-	function __construct($nombre,$e,$nota,$importe)
+	function __construct($nombre, $e, $nota, $importe)
 	{
-		parent::__construct($nombre,$e,$nota);
-		$this->importebeca=$importe;
-	}	
+		parent::__construct($nombre, $e, $nota);
+		$this->importebeca = $importe;
+	}
 	function visualizarbecado()
 	{
 		echo "<br>";
 		parent::visualizar();
 		echo $this->importebeca;
-		
 	}
 	function aprobar()
 	{
 		echo "<br> Pasando por aqui";
-	}	
+	}
 } //fin de la clase Becado
-
-
-?>
