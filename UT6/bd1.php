1575cf4b3bee0fe7ef6051c08d9c2d757642279c
Mori <64525549+angelidito@users.noreply.github.com>
2020-12-22 10:12:41 +0100

    22-12-2020

--- UT6/bd1.php
+++ UT6/bd1.php
@@ -1,21 +1,24 @@
 <?php
 
-$db_host="localhost:3308";
-$db_usuario="root";
-$db_clave="";
-$db_nombre="ciclos";
+$db_host = "localhost:8080";
+$db_usuario = "root";
+$db_clave = "";
+$db_nombre = "ciclos";
 
-$conexion=mysqli_connect($db_host,$db_usuario,$db_clave,$db_nombre);
-if (mysqli_connect_errno())
-{
+$conexion = mysqli_connect($db_host, $db_usuario, $db_clave, $db_nombre);
+if (mysqli_connect_errno()) {
 	echo "Fallo en la conexión";
 	exit();
 }
-mysqli_select_db ($conexion,$db_nombre) or die ("No se encontró la BD");
-$consulta="select * from alumno";
-$resultados=mysqli_query($conexion,$consulta);
-while ($fila=mysqli_fetch_row($resultados))
-	{	
-	echo $fila[0]." ".$fila[1]." ".$fila[2]." ".$fila[3]."<br>";
-	}
-?>
+mysqli_select_db($conexion, $db_nombre) or die("No se encontró la BD");
+$consulta = "select * from alumno";
+$resultados = mysqli_query($conexion, $consulta);
+while ($fila = mysqli_fetch_row($resultados)) {
+	echo $fila[0] . " " . $fila[1] . " " . $fila[2] . " " . $fila[3] . "<br>";
+}
+
+
+// ACTIVIDAD 1: Amplia la BD ciclo, con una nueva tabla denominada módulo. Tendrá como atributo el código del módulo (denominación abreviada) y la calificación final obtenida por un alumno. Los tipos de los campos serán los que tú consideres oportunos.
+// Dado que la calificación del módulo depende del alumno, piensa en qué otra ampliación necesita tu BD e impleméntala desde php.
+// Además deberás insertar información en las nuevas tablas y por último mostrar los alumnos que estén aprobados en el módulo de DWES.
+// Todo ello desde php utilizando las funciones vistas anteriormente.
