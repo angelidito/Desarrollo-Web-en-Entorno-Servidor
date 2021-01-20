cad740c683ed2181a5bbd3b79b93885c6a77748c
Mori <64525549+angelidito@users.noreply.github.com>
2021-01-20 14:19:08 +0100

    Update ActividadUT6_guiada.php

--- UT6/bbdd/Actividades/ActividadUT6_guiada.php
+++ UT6/bbdd/Actividades/ActividadUT6_guiada.php
@@ -1,11 +1,11 @@
 <?php
 
-$db_host = "localhost:8080";
+$db_host = "localhost:3307";
 $db_usuario = "root";
 $db_clave = "";
 $db_nombre = "ciclos";
 
-$conexion = mysqli_connect($db_host, $db_usuario, $db_clave, $db_nombre);
+$conexion = mysqli_connect($db_host, $db_usuario, $db_clave);
 if (mysqli_connect_errno()) {
 	echo "Fallo en la conexión";
 	exit();
@@ -20,7 +20,11 @@ while ($fila = mysqli_fetch_row($resultados)) {
 
 // ACTIVIDAD 1: Amplia la BD ciclo, con una nueva tabla denominada módulo. Tendrá como atributo el código del módulo (denominación abreviada) y la calificación final obtenida por un alumno. Los tipos de los campos serán los que tú consideres oportunos.
 // Dado que la calificación del módulo depende del alumno, piensa en qué otra ampliación necesita tu BD e impleméntala desde php.
+$crear = "create table módulo ( 
+	cod int primary key AUTO_INCREMENT, 
+	cali int,
+	FOREIGN KEY(cali) REFERENCES alumno(id_al));";
+
+mysqli_query($conexion, $crear);
 // Además deberás insertar información en las nuevas tablas y por último mostrar los alumnos que estén aprobados en el módulo de DWES.
 // Todo ello desde php utilizando las funciones vistas anteriormente. 
-
-
