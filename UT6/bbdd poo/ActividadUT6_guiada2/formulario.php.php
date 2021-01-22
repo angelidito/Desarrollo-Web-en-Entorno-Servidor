4eee4b09948aa4dbac3b7811b219a73772ffd49f
Mori <64525549+angelidito@users.noreply.github.com>
2021-01-22 13:57:47 +0100

    Viernes 22

---
+++ UT6/bbdd poo/ActividadUT6_guiada2/formulario.php.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario</title>
+</head>
+
+<body>
+    <br>
+    <br>
+    <br>
+    <form action="datosimg.php" method="post" enctype="multipart/form-data">
+        <table border=1 align="center" bgcolor="olive">
+    <tr>
+        <td>Denominación: </td>
+        <td><input type="text" name="texto_id"></td>
+    </tr>
+    <tr>
+        <td>Imagen: </td>
+        <td><input type="text" name="imagen1" size="20"></td>
+    </tr>
+    <tr>
+        <td colspan="2" ><input type="submit" name=""></td>
+    </tr>
+        </table>
+    </form>
+</body>
+
+</html>
